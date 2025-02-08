68c99e61d3926410a09ad5bf80e80c6439a25b24
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-02-08 15:48:41 -0300

    feat: improving project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header.jsx";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescritptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescritptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({title, description, image}) {
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
 
 function App() {
   return (
@@ -40,7 +10,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map(concept => <CoreConcept {...concept} />)}
+            {CORE_CONCEPTS.map(concept => <CoreConcept key={concept.title} {...concept} />)}
           </ul>
         </section>
       </main>
