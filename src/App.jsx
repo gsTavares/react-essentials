c864e440f646029b2a022bd755e8911172eec36b
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-02-08 15:43:46 -0300

    feat: added core concepts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescritptions = ["Fundamental", "Crucial", "Core"];
 
@@ -40,9 +40,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            <CoreConcept title="Components" description="The core UI building block" image={componentsImg}/>
-            <CoreConcept />
-            <CoreConcept />
+            {CORE_CONCEPTS.map(concept => <CoreConcept {...concept} />)}
           </ul>
         </section>
       </main>
