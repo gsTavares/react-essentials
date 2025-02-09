dbc439f4efea62909c4ca2d3ec1f7d636b1c01b8
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-02-09 10:14:44 -0300

    feat: conditional rendering example

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -31,15 +31,20 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {!selectedTopic && <p>Please selected a topic.</p>}
+          {selectedTopic &&
+            (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+            )
+          }
         </section>
       </main>
     </div>
