bac6619de01c41a87b602c8c8e2dbdafb5bf8392
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-02-08 23:17:53 -0300

    feat: component composition example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from "./components/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
@@ -12,6 +13,15 @@ function App() {
           <ul>
             {CORE_CONCEPTS.map(concept => <CoreConcept key={concept.title} {...concept} />)}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
